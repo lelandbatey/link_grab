d92d4bc63052f9f5b36818cb3a55b2a8829ae0ab
lelandbatey <lelandbatey@gmail.com>
2015-01-06 17:54:14 -0800

    Auto-detection of cURL libs during compilation

--- Makefile
+++ Makefile
@@ -2,20 +2,43 @@
 BIN_DIR = ~/bin/
 OUTPUT_NAME = link_grab
 COMPILER = g++ -g -std=c++11
+LIBS = -lcurl
+GIT_UPDATE_SUBMODULE = git submodule init && git submodule update && git submodule foreach git pull origin master
+
+# Tests if cURL is installed, and if not, compiles the program without it.
+CURL_TEST:=$(shell echo '\#include <curl/curl.h>' | g++ -E -xc++ - > /dev/null 2>&1; echo $$? )
+ifneq ($(CURL_TEST),0)
+	COMPILER+= -DNO_CURL
+	LIBS=
+endif
+ifneq ($(CURL_TEST),0)
+$(info cURL not installed. Program will be compiled without cURL support. HTML must be piped via STDIN to be interpreted.)
+endif
+ifeq ($(CURL_TEST),0)
+$(info cURL library installed.)
+endif
+
 
 all: executable
 
 html_parser/html_parser.cpp:
-	git submodule init && git submodule update && git submodule foreach git pull origin master
+	$(GIT_UPDATE_SUBMODULE)
 
-executable: html_parser/html_parser.cpp
-	$(COMPILER) main.cpp -lcurl -o $(OUTPUT_NAME)
+arg_parse/arg_parse.cpp:
+	$(GIT_UPDATE_SUBMODULE)
+
+executable: html_parser/html_parser.cpp arg_parse/arg_parse.cpp
+	$(COMPILER) main.cpp $(LIBS) -o $(OUTPUT_NAME)
+
 
 clean:
 	rm -fv $(OUTPUT_NAME)
 
-debug: COMPILER = g++ -DDEBUG -g -std=c++11 -Wall
+debug: COMPILER+= -DDEBUG
 debug: clean executable
+
+nocurl: COMPILER+= -DNO_CURL
+nocurl: clean executable
 
 release: clean executable
 
--- main.cpp
+++ main.cpp
@@ -1,21 +1,85 @@
 #include <iostream>
+#include <iterator>
+#include <istream>
+#include <ostream>
 #include <string>
 #include <map>
+
+#include <unistd.h> /* isatty() */
+
 #include "./html_parser/html_parser.cpp"
-#include "request.cpp"
+#include "./arg_parse/arg_parse.cpp"
 #include "url_parse.cpp"
 
+#ifndef NO_CURL
+#define NO_CURL 0
+#endif
+
+#if NO_CURL
+#else
+#include "request.cpp"
+#endif
+
+// This function originally found here:
+//     http://stackoverflow.com/a/202097
+std::string read_stdin(){
+	// don't skip whitespace while reading
+	std::cin >> std::noskipws;
+
+	// use stream iterators to copy the stream to a string
+	std::istream_iterator<char> it(std::cin);
+	std::istream_iterator<char> end;
+	std::string result(it, end);
+
+	return result;
+}
 
 int main(int argc, char const *argv[]){
-	std::string url;
-	if (argc > 1){
+	auto url = std::string("");
+	auto args = argparse::ArgumentParser(argc, argv);
+
+	// Use the argument parser to discern which url the user has specified.
+	if (args.option_exists("url")){
+		url = args.get_option("url");
+	} else if (argc == 2){
 		url = argv[1];
 	} else {
+		// Search for bare args that might be urls. The first one we find that
+		// matches will be the one used.
+		auto bare_args = args.get_bare_args();
+		for (unsigned int i = 0; i < bare_args.size(); ++i){
+			if (bare_args[i].find("//") != std::string::npos){
+				url = bare_args[i];
+				break;
+			} else if (bare_args[i].find(".") != std::string::npos){
+				url = bare_args[i];
+				break;
+			}
+		}
+	}
+	if (url.empty() && argc > 1){
 		std::cout << "Please enter a url." << std::endl;
 		return 0;
+	} else if (argc == 1){
+		url = "stdin";
 	}
 
-	char* page = handle_url(url.c_str());
+	// Check if the user want's us to read from stdin.
+	const char* page;
+	if (args.option_exists("stdin")){
+		page = read_stdin().c_str();
+	} else if (url == "stdin"){
+		page = read_stdin().c_str();
+	} else {
+#if NO_CURL
+		std::cerr << 
+		"Compiled without cURL support. This program cannot fetch pages on it's own.\n" << 
+		"Pipe HTML into stdin to extract links." << std::endl;
+		return 0;
+#else
+		page = handle_url(url.c_str());
+#endif
+	}
 
 	std::string test_doc(page);
 
